22047216bc4159f59a81d4f54000941881247256
hannamarcus <106893601+hannamarcus@users.noreply.github.com>
2022-06-23 23:54:12 -0700

    ain

--- 03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
+++ 03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
@@ -1,21 +1,6 @@
 // Change the values and operators below to test your algorithm meets all conditions
 var x = 50;
 var expression1 = (x < 25);
-var expression2 = (x >= 50);
+var expression2 = (x > 50);
 
 // Write Your JavaScript Code Here
-if (expression1, expression2 === true) {
-    console.log("True ✅ True ✅");
-  } 
-
-  if (expression1 === true) {
-    console.log("True ✅ False ❌");
-  } 
-
-  if (expression2 === true) {
-    console.log("True ✅ False ❌");
-  } 
-
-  if (expression1, expression2 === false) {
-    console.log("False ❌ False ❌");
-  } 
