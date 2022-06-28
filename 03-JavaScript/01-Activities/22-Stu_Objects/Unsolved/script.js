0985ce4614020d748cdec5eb405f8851aa5c2726
hannamarcus <106893601+hannamarcus@users.noreply.github.com>
2022-06-27 19:48:56 -0700

    ain

--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,18 @@
 //WRITE YOUR CODE BELOW
+
+var customerOrder = {
+    drinkName: "coffee",
+    sugars: 2,
+    isReady: false
+}
+
+console.log(customerOrder)
+
+console.log("drinkName", customerorder.drinkName, sugars, customerOrder.sugars)
+
+if(customerOrder === true) {
+    console.log("Ready for pick-up")
+else {
+    console.log("Still in the queue")
+    }
+}
