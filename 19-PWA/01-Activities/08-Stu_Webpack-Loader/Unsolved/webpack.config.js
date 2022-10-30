aea9aeb96e61d5c7a2797666d407dfba6d45fe1b
Hanna Marcus <hannamarcus1@gmail.com>
2022-10-30 08:52:37 +0100

    class push

--- 19-PWA/01-Activities/08-Stu_Webpack-Loader/Unsolved/webpack.config.js
+++ 19-PWA/01-Activities/08-Stu_Webpack-Loader/Unsolved/webpack.config.js
@@ -22,7 +22,6 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env']
           }
